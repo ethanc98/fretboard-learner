8b2c40cb438cf70fc22aed5b07b29c66825d1161
Ethan Calnan <ethanjamescalnan@gmail.com>
2022-02-09 18:55:40 +0000

    added start script

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
